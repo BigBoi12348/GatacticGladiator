309b88099945bf2833928c4093315b960a602944
Ragernator <88657541+Ragernator@users.noreply.github.com>
2023-03-07 15:21:32 +1100

    Small

---
+++ Gladiator/Assets/1_Scripts/Enemy Script/EnemyBehaviour.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyBehaviour : MonoBehaviour
+{   
+    Transform _playerTransform;
+    [Header("Enemy Movement")]
+    [SerializeField] private float _enemyMovementSpeed;
+    [SerializeField] private float _enemyMovementSpeedModifier;
+    
+
+    [Header("Bleed Out values")]
+    private bool _bodyPartMissing;
+    [SerializeField] private float _totalBleedOut;
+    [SerializeField] private float _bleedOutStrength;
+    [SerializeField] private float _bleedOutModifier;
+    private bool _alreadyDead;
+    
+
+    [Header("Body Parts")]
+    [SerializeField] private EnemyBodyPart[] enemyBodyParts;
+
+
+    [Header("References")]
+    [SerializeField] private AIDestinationSetter _aIDestinationSetter;
+    [SerializeField] private AIPath _aIPath;
+    [SerializeField] private Collider _groundDetectCollider;
+
+    private void Awake() 
+    {
+        foreach (var enemyBodyPart in enemyBodyParts)
+        {
+            enemyBodyPart.Init(this);
+        }
+    }
+
+    public void Init(Transform playerTarget)
+    {
+        _playerTransform = playerTarget;
+        _aIDestinationSetter.target = playerTarget;
+    }
+    
+    private void Start() 
+    {
+        _aIPath.maxSpeed = _enemyMovementSpeed;
+    }
+
+    private void Update()
+    {
+        if(_bodyPartMissing)
+        {
+            if(_totalBleedOut > 0)
+            {
+                _totalBleedOut -= _bleedOutStrength + _bleedOutModifier;
+            }
+            else
+            {
+                Death();
+            }
+        }
+        
+        transform.LookAt(_playerTransform);
+        //Fix Enemy Rotation
+        // float angle = Vector3.Angle(transform.position, _playerTransform.position);
+        // Debug.Log(angle);
+        // transform.localEulerAngles = new Vector3(0,angle,0);
+    }
+
+
+    public void BodyPartLost(bool doIDie, float bleedingStrength)
+    {
+        if(!_alreadyDead)
+        {
+            if(doIDie)
+            {
+                Death();
+            }
+            else
+            {
+                _bodyPartMissing = true;
+                _bleedOutModifier += bleedingStrength;
+            }
+        }
+    }
+
+    private void Death()
+    {
+        _alreadyDead = true;
+        _aIPath.canMove = false;
+        _aIDestinationSetter.enabled = false;
+        //_groundDetectCollider.enabled = false;
+        InGameLevelManager.Instance.EnemyHasDied();
+        Destroy(this);
+    }
+}
---
+++ Gladiator/Assets/1_Scripts/Enemy Script/EnemySpawnerManager.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerManager : MonoBehaviour
+{
+    [SerializeField] private Transform _playerTransform;
+
+    [Header("Enemy Spawn Control")]
+    [SerializeField] private bool _canSpawn;
+    [SerializeField] private float _spawnInterval;
+    private float _spawnTimer;
+    [SerializeField] private int _currentDifficultyRank;
+
+
+    [Header("Enemy Control")]
+    [SerializeField] private Transform _enemyContainer;
+    [SerializeField] private Transform[] _spawnLocations;
+    public int TotalEnemiesSpawningThisRound{get; private set;}
+    
+
+    [Header("Enemies")]
+    [SerializeField] private Enemy[] enemyTypes;
+    private List<GameObject> _enemiesToSpawn = new List<GameObject>();
+    private List<EnemyBehaviour> _enemyBehaviours = new List<EnemyBehaviour>();
+
+    private void OnEnable() 
+    {
+        GameEvents.gameStartSetUp += GameIsStarting;
+        GameEvents.playerStartGame += PlayerHasStartedGame;
+        GameEvents.gameEndSetUp += GameEndSetUp;
+        GameEvents.playerFinsihedGame += EndOfRound;
+    }
+
+    private void OnDisable() 
+    {
+        GameEvents.gameStartSetUp -= GameIsStarting;
+        GameEvents.gameEndSetUp -= GameEndSetUp;
+        GameEvents.playerFinsihedGame -= EndOfRound;
+    }
+
+    void Start()
+    {
+        //_canSpawn = false;
+        if(RoundData.DifficultyRank > 0)
+        {
+            _currentDifficultyRank = RoundData.DifficultyRank;
+        }
+        else
+        {
+            _currentDifficultyRank = 10;
+        }
+    }
+
+    void Update()
+    {
+        if(_canSpawn)
+        {
+            if(_spawnTimer <= 0)
+            {
+                if(_enemiesToSpawn.Count > 0)
+                {
+                    SpawnEnemy(_enemiesToSpawn[0], _spawnLocations[Random.Range(0,_spawnLocations.Length)].position);
+                    _enemiesToSpawn.RemoveAt(0);
+                    _spawnTimer = _spawnInterval;
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                _spawnTimer -= Time.deltaTime;
+            }
+        }
+    }
+    
+    private void GameIsStarting()
+    {
+        Mathf.Pow(_currentDifficultyRank, 1.2f);
+
+        int tempDifficultyScore = _currentDifficultyRank;
+        int tempCurrentRound = InGameLevelManager.Instance.CurrentRound;
+        _enemiesToSpawn = new List<GameObject>();
+
+        while (tempDifficultyScore > 0)
+        {
+            List<Enemy> tempEnemyToChoseFrom = new List<Enemy>();
+            foreach (var enemy in enemyTypes)
+            {
+                if(enemy.StartSpawnWave <= tempCurrentRound)
+                {
+                    tempEnemyToChoseFrom.Add(enemy);
+                }
+            }
+            int randNum = Random.Range(0,tempEnemyToChoseFrom.Count);
+
+            GameObject tempEnemyObj = tempEnemyToChoseFrom[randNum].EnemyObj;
+            tempDifficultyScore -= tempEnemyToChoseFrom[randNum].DifficultyRank;
+            _enemiesToSpawn.Add(tempEnemyObj);
+            TotalEnemiesSpawningThisRound++;
+        }
+
+        //Debug.Log("excuse");
+        RoundData.DifficultyRank = _currentDifficultyRank;
+        GameEvents.playerStartGame?.Invoke();
+    }
+
+    private void PlayerHasStartedGame()
+    {
+        //Debug.Log("NO way");
+        _canSpawn = true;
+    }
+
+    private void GameEndSetUp()
+    {
+        
+    }
+
+    private void EndOfRound()
+    {
+        foreach (Transform enemyTransform in _enemyContainer)
+        {
+            Destroy(enemyTransform.gameObject);
+        }
+    }
+
+    private void SpawnEnemy(GameObject enemyToSpawn, Vector3 locationToSpawn)
+    {
+        GameObject tempEnemyObj = Instantiate(enemyToSpawn, locationToSpawn, Quaternion.identity, _enemyContainer);
+        EnemyBehaviour tempEnemyBehaviour = tempEnemyObj.GetComponent<EnemyBehaviour>();
+        tempEnemyBehaviour.Init(_playerTransform);
+        _enemyBehaviours.Add(tempEnemyBehaviour);
+    }
+}
+
+
+[System.Serializable]
+public class Enemy
+{
+    public GameObject EnemyObj;
+    public int DifficultyRank;
+    [Tooltip("Limit when they can spawn until the wave coutner reaches it")]
+    public int StartSpawnWave;
+}
