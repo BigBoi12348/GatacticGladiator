7c1390461963602955473db840763fc68bf51d78
Ragernator <88657541+Ragernator@users.noreply.github.com>
2023-03-09 15:09:23 +1100

    Buttom eid

--- Gladiator/Assets/1_Scripts/Upgrade Scene Scripts/UpgradeSceneManager.cs
+++ Gladiator/Assets/1_Scripts/Upgrade Scene Scripts/UpgradeSceneManager.cs
@@ -5,12 +5,27 @@ using UnityEngine.UI;
 
 public class UpgradeSceneManager : MonoBehaviour
 {
+    [Header("Buttons")]
+    [SerializeField] private Button _inventoryButton;
+    [SerializeField] private Button _upgradesButton;
     [SerializeField] private Button _exitButton;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
+        _inventoryButton.onClick.AddListener(GoToInventoryScreen);
+        _upgradesButton.onClick.AddListener(GoToUpgradesScreen);
         _exitButton.onClick.AddListener(GoBackToGame);
+    }
+
+    private void GoToInventoryScreen()
+    {
+        ViewManager.Show<InventoryView>(0);
+    }
+
+    private void GoToUpgradesScreen()
+    {
+        ViewManager.Show<UpgradesView>(0);
     }
     
     private void GoBackToGame()
