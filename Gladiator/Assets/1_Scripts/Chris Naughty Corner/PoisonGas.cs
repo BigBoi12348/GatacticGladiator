55d14b1120d1d12f0111777ea13bad1ad01a0155
BigBoi12348 <107095940+BigBoi12348@users.noreply.github.com>
2023-03-30 14:28:20 +1100

    Poisons done

--- Gladiator/Assets/1_Scripts/Chris Naughty Corner/PoisonGas.cs
+++ Gladiator/Assets/1_Scripts/Chris Naughty Corner/PoisonGas.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class PoisonGas : MonoBehaviour
 {
-    //public float damageAmount = 10.0f; // amount of damage per second
-    //public float damageInterval = 1.0f; // interval in seconds between damage ticks
+    public float damagePerSecond = 10.0f; // Amount of damage to apply per second
+    public float damageInterval = 1.0f; // Interval in seconds between damage ticks
 
-    //private float timeSinceLastDamage = 0.0f;
+    private float timeSinceLastDamage = 0.0f;
 
-    //private void OnTriggerStay(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        timeSinceLastDamage += Time.deltaTime;
-    //        if (timeSinceLastDamage >= damageInterval)
-    //        {
-    //            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
-    //            if (playerHealth != null)
-    //            {
-    //                playerHealth.TakeDamage(damageAmount);
-    //            }
-    //            timeSinceLastDamage -= damageInterval;
-    //        }
-    //    }
-    //}
+    private void OntriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            timeSinceLastDamage += Time.deltaTime;
+            if (timeSinceLastDamage >= damageInterval)
+            {
+                PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+                if (playerHealth != null)
+                {
+                    playerHealth.TakeDamage(Mathf.RoundToInt(damagePerSecond * damageInterval));
+                    Debug.Log("Player health: " + playerHealth.currentHealth); // Debug statement
+                }
+                timeSinceLastDamage -= damageInterval;
+            }
+        }
+    }
 }
