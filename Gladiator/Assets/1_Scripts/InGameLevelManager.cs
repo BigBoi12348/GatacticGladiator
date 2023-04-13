fb7b459baf74ed311c6bbe5348497bbdb86cf34d
Ragernator <88657541+Ragernator@users.noreply.github.com>
2023-04-13 13:12:24 +1000

    v

--- Gladiator/Assets/1_Scripts/InGameLevelManager.cs
+++ Gladiator/Assets/1_Scripts/InGameLevelManager.cs
@@ -9,7 +9,7 @@ public class InGameLevelManager : MonoBehaviour
 
     public int _totalEnemiesCounter{get; private set;}
     public int CurrentRound{get; private set;}
-    public GameObject[] levels;
+    //public GameObject[] levels;
 
 
     [Header("References")]
@@ -62,12 +62,12 @@ public class InGameLevelManager : MonoBehaviour
 
     private void GameIsStarting()
     {
-        for(int i = 0; i < levels.Length; i++)
-        {
-            levels[i].SetActive(false);
-        }
-        CurrentRound = RoundData.Wave;
-        levels[Random.Range(0, 3)].SetActive(true);
+        // for(int i = 0; i < levels.Length; i++)
+        // {
+        //     levels[i].SetActive(false);
+        // }
+        // CurrentRound = RoundData.Wave;
+        // levels[Random.Range(0, 3)].SetActive(true);
     }
 
     private void GameStarted()
