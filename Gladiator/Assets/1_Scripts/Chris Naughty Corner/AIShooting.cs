d71e2440838dfaa4dd74ed0a2b348a58c513d59a
BigBoi12348 <107095940+BigBoi12348@users.noreply.github.com>
2023-04-18 00:13:47 +1000

    Fixed ai and turretnt

--- Gladiator/Assets/1_Scripts/Chris Naughty Corner/AIShooting.cs
+++ Gladiator/Assets/1_Scripts/Chris Naughty Corner/AIShooting.cs
@@ -1,30 +1,143 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIShooting : MonoBehaviour
 {
+    //public Transform player;
+    //public float shootingRange = 10f;
+    //public float shootingDelay = 1f;
+    //public GameObject bulletPrefab;
+    //public Transform bulletSpawnPoint;
+
+    //private float lastShotTime;
+
+    //void Start()
+    //{
+    //    lastShotTime = Time.time;
+    //}
+
+    //void Update()
+    //{
+    //    if (Vector3.Distance(transform.position, player.position) < shootingRange && Time.time - lastShotTime > shootingDelay)
+    //    {
+    //        transform.LookAt(player.position);
+    //        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+    //        bullet.GetComponent<Rigidbody>().velocity = (player.position - bullet.transform.position).normalized * 10;
+    //        lastShotTime = Time.time;
+    //    }
+    //}
+
+    public NavMeshAgent agent;
+
     public Transform player;
-    public float shootingRange = 10f;
-    public float shootingDelay = 1f;
-    public GameObject bulletPrefab;
-    public Transform bulletSpawnPoint;
 
-    private float lastShotTime;
+    public LayerMask whatIsGround, whatIsPlayer;
 
-    void Start()
+    public float health;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
     {
-        lastShotTime = Time.time;
+        player = GameObject.Find("Target").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    void Update()
+    private void Update()
     {
-        if (Vector3.Distance(transform.position, player.position) < shootingRange && Time.time - lastShotTime > shootingDelay)
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
         {
-            transform.LookAt(player.position);
-            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
-            bullet.GetComponent<Rigidbody>().velocity = (player.position - bullet.transform.position).normalized * 10;
-            lastShotTime = Time.time;
+            ///Attack code here
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.Euler(0,90,0)).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            ///End of attack code
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
