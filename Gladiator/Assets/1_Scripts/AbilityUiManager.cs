335858ca19792b56b1258e203a22d8ca40703a47
Ragernator <88657541+Ragernator@users.noreply.github.com>
2023-04-20 21:55:05 +1000

    Small Battle Scene Change

--- Gladiator/Assets/1_Scripts/AbilityUiManager.cs
+++ Gladiator/Assets/1_Scripts/AbilityUiManager.cs
@@ -8,17 +8,29 @@ public class AbilityUiManager : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private FirstPersonController _firstPersonController;
-    
+
     [Header("UI Objects")]
     [SerializeField] private GameObject _firstAbility;
     [SerializeField] private GameObject _secondAbility;
     [SerializeField] private GameObject _thirdAbility;
     [SerializeField] private GameObject _fourthAbility;
     [SerializeField] private GameObject _fifthAbility;
-
+    bool secondOpen;
+    bool thirdOpen;
+    bool fourthOpen;
+    bool fifthOpen;
+    
     [Header("Ability UI")]
     [SerializeField] private Slider _firstAbilitySlider;
     [SerializeField] private TMP_Text _firstTextTimer;
+    [SerializeField] private Slider _secondAbilitySlider;
+    [SerializeField] private TMP_Text _secondTextTimer;
+    [SerializeField] private Slider _thirdAbilitySlider;
+    [SerializeField] private TMP_Text _thirdTextTimer;
+    [SerializeField] private Slider _fourthAbilitySlider;
+    [SerializeField] private TMP_Text _fourthTextTimer;
+    [SerializeField] private Slider _fifthAbilitySlider;
+    [SerializeField] private TMP_Text _fifthTextTimer;
 
     void Start()
     {
@@ -30,18 +42,22 @@ public class AbilityUiManager : MonoBehaviour
         if(PlayerUpgradesData.StarTwo)
         {
             _secondAbility.SetActive(true);
+            secondOpen = true;
         }
         if(PlayerUpgradesData.StarThree)
         {
             _thirdAbility.SetActive(true);
+            thirdOpen = true;
         }
         if(PlayerUpgradesData.StarFour)
         {
             _fourthAbility.SetActive(true);
+            fourthOpen = true;
         }
         if(PlayerUpgradesData.StarFive)
         {
             _fifthAbility.SetActive(true);
+            fifthOpen = true;
         }
 
         if(_firstPersonController == null)
@@ -56,5 +72,25 @@ public class AbilityUiManager : MonoBehaviour
     {
         _firstAbilitySlider.value = _firstPersonController.dashCooldownTimer;
         _firstTextTimer.text = Mathf.RoundToInt(_firstPersonController.dashCooldownTimer).ToString();
+        if(secondOpen)
+        {
+            _secondAbilitySlider.value = _firstPersonController.dashCooldownTimer;
+            _secondTextTimer.text = Mathf.RoundToInt(_firstPersonController.dashCooldownTimer).ToString();
+        }
+        if(thirdOpen)
+        {
+            _thirdAbilitySlider.value = _firstPersonController.dashCooldownTimer;
+            _thirdTextTimer.text = Mathf.RoundToInt(_firstPersonController.dashCooldownTimer).ToString();
+        }
+        if(fourthOpen)
+        {
+            _fourthAbilitySlider.value = _firstPersonController.dashCooldownTimer;
+            _fourthTextTimer.text = Mathf.RoundToInt(_firstPersonController.dashCooldownTimer).ToString();
+        }
+        if(fifthOpen)
+        {
+            _fifthAbilitySlider.value = _firstPersonController.dashCooldownTimer;
+            _fifthTextTimer.text = Mathf.RoundToInt(_firstPersonController.dashCooldownTimer).ToString();
+        }
     }
 }
